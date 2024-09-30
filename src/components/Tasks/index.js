527727aaeb3f1d7b9294c7a58e9b89be9ad44a26
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2024-09-30 19:36:27 -0300

    Aula 02: Componente Task

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,7 +1,39 @@
 import { Text } from '../Text';
 
+import pending from '../../assets/images/pending.png';
+import done from '../../assets/images/done.png';
+import excluir from '../../assets/images/delete.png';
+import edit from '../../assets/images/edit.png';
+
+import { Task, TaskHeader, TaskDescription, TaskFooter, TaskStatus, TaskActions, TaskIcon } from './styles';
+import { TouchableOpacity } from 'react-native';
+
 export default function Tasks() {
   return (
-    <Text>Lista de tarefas</Text>
+    <Task>
+      <TaskHeader>
+        <Text size={18} weight="600">Estudar React Native</Text>
+      </TaskHeader>
+      <TaskDescription>
+        <Text opacity={0.5}>Estudar para o Primeiro Simulado</Text>
+      </TaskDescription>
+      <TaskFooter>
+        <TaskStatus onPress={() => alert('Alterar Status da Tarefa')}>
+          <TaskIcon source={pending} />
+
+          <Text color="#E620AE">Pendente</Text>
+        </TaskStatus>
+
+        <TaskActions>
+          <TouchableOpacity onPress={() => alert('Alterar Tarefa')}>
+            <TaskIcon source={edit} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => alert('Excluir Tarefa')} >
+            <TaskIcon source={excluir} />
+          </TouchableOpacity>
+        </TaskActions>
+      </TaskFooter>
+    </Task >
   );
 }
