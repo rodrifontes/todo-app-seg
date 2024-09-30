0547497ba6172084954a9bda411e2d50587e2529
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2024-09-30 19:36:37 -0300

    Aula 02

---
+++ src/components/Tasks/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const Task = styled.View`
+  margin: 24px 20px 0;
+  padding: 24px;
+  border-color: #EEEEEE;
+  border-width: 1px;
+  border-radius: 12px;
+  gap: 24px;
+`;
+
+export const TaskHeader = styled.View``;
+
+export const TaskDescription = styled.View``;
+
+export const TaskFooter = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  
+`;
+
+export const TaskStatus = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const TaskActions = styled.View`
+  flex-direction: row;
+  gap: 16px;
+`;
+
+export const TaskIcon = styled.Image`
+  width: 16px;
+  height: 16px;
+  resize-mode: contain;
+`;
